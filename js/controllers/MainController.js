19882d591872ee36ee876be0f48d7ac9481fe639
Dale Myszewski II <dale.myszewski@gmail.com>
2016-03-15 10:01:05 -0500

    Added sensible commentary. :)

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -9,29 +9,32 @@ app.controller('MainController', ['$scope', '$http', 'aquestion', function($scop
     $scope.question = data[0];
   });
 
+  //Trigged by the user hitting the answer question button
   $scope.answerBtn = function () {
     likeness_points = 0;
     possible_points = 0; //not sure if this will be used...this could also be a pain
     your_answer = $scope.answer.toLowerCase().split(" ")
     the_answer = $scope.question.answer.toLowerCase().split(" ")
 
+    //get a total possible points score - not in use yet
     for (i = 0; i < the_answer.length; i++)
     {
-      //get a total possible points score
       if (your_answer[i] != "the" || your_answer[i] != "an")
       {
         possible_points += 1;
       }
     }
 
+    //get a score to see how alike the two answers are
     for (i = 0; i < your_answer.length; i++)
     {
       if ((your_answer[i] != "the" || your_answer[i] != "an") && $.inArray(your_answer[i], the_answer) != -1)
       {
-        likeness_points += 1; //so basically if you get one word right I am going to give you credit. :D
+        likeness_points += 1;
       }
     }
 
+    //so basically if you get one word right I am going to give you credit. :D
     if (likeness_points >= 1) //original code - $scope.answer.toLowerCase() == $scope.question.answer.toLowerCase()
     {
       $scope.answer = "Correct! Your answer was: "+  +" The answer was: " + $scope.question.answer;
@@ -47,6 +50,7 @@ app.controller('MainController', ['$scope', '$http', 'aquestion', function($scop
     };
   };
 
+  //gets a new question...should this be a factory?? *Shrug*
   $scope.newQuestion = function (){
     $scope.answer = "";
     $scope.showanswer = false;
